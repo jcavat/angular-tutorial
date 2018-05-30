1146681cf50d19d70658d187eefb07e983fb0b66
Joel Cavat <jcavat@gmail.com>
2018-05-30 21:05:41 +0200

    Add capitalized pipe

--- orders/src/app/app.module.ts
+++ orders/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { VendorsComponent } from './vendors/vendors.component';
 import { OrderRepositoryService, FakeOrderRepositoryService, RestOrderRepositoryService } from './vendors/order-repository.service';
 import { HttpClientModule } from '@angular/common/http';
 import { VendorCardComponent } from './vendor-card/vendor-card.component';
+import { CapitalizePipe } from './capitalize.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     VendorsComponent,
-    VendorCardComponent
+    VendorCardComponent,
+    CapitalizePipe
   ],
   imports: [
     BrowserModule,
